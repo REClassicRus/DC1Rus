e936f855b12c6637247539e7a7643e92270142bd
C4sperPRO <148689903+C4sperPRO@users.noreply.github.com>
2024-09-14 12:48:21 +0300

    Patcher

---
+++ Utils/Patcher/Unit1.dfm
@@ -0,0 +1,230 @@
+object Main: TMain
+  Left = 192
+  Top = 125
+  BorderIcons = [biSystemMenu, biMinimize]
+  BorderStyle = bsSingle
+  Caption = #1055#1077#1088#1077#1074#1086#1076' Dino Crisis'
+  ClientHeight = 472
+  ClientWidth = 839
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  Position = poScreenCenter
+  OnClose = FormClose
+  OnCreate = FormCreate
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 8
+    Top = 390
+    Width = 3
+    Height = 13
+    Caption = '-'
+  end
+  object SizeFirstLineLbl: TLabel
+    Left = 813
+    Top = 363
+    Width = 6
+    Height = 13
+    Caption = '0'
+  end
+  object SizeSecondLineLbl: TLabel
+    Left = 813
+    Top = 418
+    Width = 6
+    Height = 13
+    Caption = '0'
+  end
+  object Label2: TLabel
+    Left = 136
+    Top = 390
+    Width = 3
+    Height = 13
+    Caption = '-'
+  end
+  object Label3: TLabel
+    Left = 800
+    Top = 13
+    Width = 6
+    Height = 13
+    BiDiMode = bdLeftToRight
+    Caption = '0'
+    ParentBiDiMode = False
+    OnClick = Label3Click
+  end
+  object Button1: TButton
+    Left = 728
+    Top = 312
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object OriginalLineEdit: TEdit
+    Left = 8
+    Top = 360
+    Width = 801
+    Height = 21
+    TabOrder = 1
+    OnChange = OriginalLineEditChange
+  end
+  object OwnLineEdit: TEdit
+    Left = 8
+    Top = 415
+    Width = 801
+    Height = 21
+    TabOrder = 2
+    OnChange = OwnLineEditChange
+  end
+  object PathEdt: TEdit
+    Left = 8
+    Top = 9
+    Width = 705
+    Height = 21
+    TabOrder = 3
+  end
+  object ListBox1: TListBox
+    Left = 8
+    Top = 40
+    Width = 169
+    Height = 265
+    ItemHeight = 13
+    TabOrder = 4
+    OnClick = ListBox1Click
+  end
+  object Button3: TButton
+    Left = 719
+    Top = 7
+    Width = 75
+    Height = 25
+    Caption = #1054#1073#1085#1086#1074#1080#1090#1100
+    TabOrder = 5
+    OnClick = Button3Click
+  end
+  object ListBox2: TListBox
+    Left = 184
+    Top = 40
+    Width = 649
+    Height = 313
+    ItemHeight = 13
+    TabOrder = 6
+    OnClick = ListBox2Click
+  end
+  object ToHexBtn: TButton
+    Left = 7
+    Top = 439
+    Width = 100
+    Height = 25
+    Caption = #1042' HEX'
+    TabOrder = 7
+    OnClick = ToHexBtnClick
+  end
+  object FileNameEdt: TEdit
+    Left = 8
+    Top = 310
+    Width = 169
+    Height = 21
+    TabOrder = 8
+  end
+  object CopyBtn: TButton
+    Left = 266
+    Top = 384
+    Width = 100
+    Height = 25
+    Caption = #1057#1082#1086#1087#1080#1088#1086#1074#1072#1090#1100
+    TabOrder = 9
+    OnClick = CopyBtnClick
+  end
+  object CopyBtn2: TButton
+    Left = 266
+    Top = 439
+    Width = 100
+    Height = 25
+    Caption = #1057#1082#1086#1087#1080#1088#1086#1074#1072#1090#1100
+    TabOrder = 10
+    OnClick = CopyBtn2Click
+  end
+  object CopyHexBtn: TButton
+    Left = 368
+    Top = 384
+    Width = 100
+    Height = 25
+    Caption = #1057#1082#1086#1087#1080#1088#1086#1074#1072#1090#1100' HEX'
+    TabOrder = 11
+    OnClick = CopyHexBtnClick
+  end
+  object ReturnStrsBtn: TButton
+    Left = 470
+    Top = 384
+    Width = 107
+    Height = 25
+    Caption = #1042#1086#1079#1074#1088#1072#1090
+    TabOrder = 12
+    OnClick = ReturnStrsBtnClick
+  end
+  object Button8: TButton
+    Left = 630
+    Top = 442
+    Width = 75
+    Height = 25
+    Caption = #1058#1077#1089#1090' '#1087#1072#1090#1095
+    TabOrder = 13
+    Visible = False
+    OnClick = Button8Click
+  end
+  object ProgressBar1: TProgressBar
+    Left = 8
+    Top = 336
+    Width = 169
+    Height = 17
+    TabOrder = 14
+  end
+  object RemSpaces: TButton
+    Left = 368
+    Top = 439
+    Width = 100
+    Height = 25
+    Caption = #1059#1073#1088#1072#1090#1100' '#1087#1088#1086#1073#1077#1083#1099
+    TabOrder = 15
+    OnClick = RemSpacesClick
+  end
+  object AddSpaces: TButton
+    Left = 470
+    Top = 439
+    Width = 105
+    Height = 25
+    Caption = #1044#1086#1073#1072#1074#1080#1090#1100' '#1087#1088#1086#1073#1077#1083#1099
+    TabOrder = 16
+    OnClick = AddSpacesClick
+  end
+  object SaveStrsBtn: TButton
+    Left = 605
+    Top = 384
+    Width = 100
+    Height = 25
+    Caption = #1057#1086#1093#1088#1072#1085#1080#1090#1100
+    TabOrder = 17
+    OnClick = SaveStrsBtnClick
+  end
+  object ReturnStrs2Btn: TButton
+    Left = 709
+    Top = 384
+    Width = 100
+    Height = 25
+    Caption = #1042#1086#1089#1089#1090#1072#1085#1086#1074#1080#1090#1100
+    TabOrder = 18
+    OnClick = ReturnStrs2BtnClick
+  end
+  object Button2: TButton
+    Left = 776
+    Top = 442
+    Width = 30
+    Height = 25
+    Caption = '?'
+    TabOrder = 19
+    OnClick = Button2Click
+  end
+end
---
+++ Utils/Patcher/Unit1.pas
@@ -0,0 +1,571 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, IniFiles, ClipBrd, ComCtrls;
+
+type
+  TMain = class(TForm)
+    Button1: TButton;
+    OriginalLineEdit: TEdit;
+    OwnLineEdit: TEdit;
+    Label1: TLabel;
+    SizeFirstLineLbl: TLabel;
+    SizeSecondLineLbl: TLabel;
+    PathEdt: TEdit;
+    ListBox1: TListBox;
+    Button3: TButton;
+    ListBox2: TListBox;
+    ToHexBtn: TButton;
+    FileNameEdt: TEdit;
+    CopyBtn: TButton;
+    CopyBtn2: TButton;
+    Label2: TLabel;
+    CopyHexBtn: TButton;
+    ReturnStrsBtn: TButton;
+    Button8: TButton;
+    ProgressBar1: TProgressBar;
+    RemSpaces: TButton;
+    AddSpaces: TButton;
+    SaveStrsBtn: TButton;
+    ReturnStrs2Btn: TButton;
+    Label3: TLabel;
+    Button2: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure OwnLineEditChange(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure ListBox1Click(Sender: TObject);
+    procedure ListBox2Click(Sender: TObject);
+    procedure ToHexBtnClick(Sender: TObject);
+    procedure CopyBtnClick(Sender: TObject);
+    procedure CopyBtn2Click(Sender: TObject);
+    procedure CopyHexBtnClick(Sender: TObject);
+    procedure ReturnStrsBtnClick(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure OriginalLineEditChange(Sender: TObject);
+    procedure RemSpacesClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure AddSpacesClick(Sender: TObject);
+    procedure SaveStrsBtnClick(Sender: TObject);
+    procedure ReturnStrs2BtnClick(Sender: TObject);
+    procedure Label3Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    function ReadTableCharToByte(Str: string): byte;
+    function WriteTableCharToByte(Str: string): byte;
+    { Private declarations }
+  public
+    procedure CheckSameLines;
+    { Public declarations }
+  end;
+
+type
+  PByteArray = ^TByteArray;
+  TByteArray = array of Byte;
+
+var
+  Main: TMain;
+  TableRead, TableWrite: TStringList;
+
+  TempBufStr1, TempBufStr2: string;
+  StrLine1, StrLine2: string;
+
+implementation
+
+{$R *.dfm}
+
+function BytesToStr(bytes: array of byte): string;
+const
+  BytesHex: array[0..15] of char = 
+    ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F');
+var
+  i, len: integer;
+begin
+  len := Length(bytes);
+  SetLength(Result, len * 2);
+  for i := 0 to len - 1 do begin
+    Result[i * 2 + 1] := BytesHex[bytes[i] shr 4];
+    Result[i * 2 + 2] := BytesHex[bytes[i] and $0F];
+  end;
+end;
+
+function RemoveBrackets(const InputString: string): string;
+var
+  OutputString: string;
+  InsideBrackets: Boolean;
+  i: Integer;
+begin
+  OutputString := '';
+  InsideBrackets := False;
+
+  for i := 1 to Length(InputString) do
+  begin
+    if InputString[i] = '{' then
+    begin
+      InsideBrackets := True;
+    end
+    else if InputString[i] = '}' then
+    begin
+      InsideBrackets := False;
+    end
+    else if not InsideBrackets then
+    begin
+      OutputString := OutputString + InputString[i];
+    end;
+  end;
+
+  Result := OutputString;
+end;
+
+procedure TMain.FormCreate(Sender: TObject);
+var
+  Ini: TIniFile;
+  SR: TSearchRec;
+  i: integer;
+  LastFile: string;
+begin
+  Application.Title:=Caption;
+
+  TableRead:=TStringList.Create;
+  TableWrite:=TStringList.Create;
+  Ini:=TIniFile.Create(ExtractFilePath(ParamStr(0)) + 'Config.ini');
+  PathEdt.Text:=Ini.ReadString('Main', 'Path', '');
+  TableRead.LoadFromFile(Ini.ReadString('Main', 'ReadTable', ''));
+  TableWrite.LoadFromFile(Ini.ReadString('Main', 'WriteTable', ''));
+  LastFile:=Ini.ReadString('Main', 'LastFile', '');
+  Ini.Free;
+
+  Button3.Click;
+
+  for i:=0 to ListBox1.Items.Count - 1 do
+    if LastFile = ListBox1.Items.Strings[i] then begin
+      ListBox1.ItemIndex:=i;
+      break;
+    end;
+
+  ListBox1Click(Sender);
+end;
+
+function TMain.ReadTableCharToByte(Str: string): byte;
+var
+  i: integer;
+begin
+  Result:=$00;
+  for i:=0 to TableRead.Count - 1 do
+    if (Copy(TableRead.Strings[i], 4, 1) = Str) then begin
+      Result:=StrToInt('$' + Copy(TableRead.Strings[i], 1, 2));
+      break;
+    end;
+end;
+
+function TMain.WriteTableCharToByte(Str: string): byte;
+var
+  i: integer;
+begin
+  Result:=$00;
+  for i:=0 to TableWrite.Count - 1 do
+    if (Copy(TableWrite.Strings[i], 4, 1) = Str) then begin
+      Result:=StrToInt('$' + Copy(TableWrite.Strings[i], 1, 2));
+      break;
+    end;
+end;
+
+function StrToBytesTBRead(Str: string): string;
+var
+  i, j: integer; KeyBytes, BytesValue: string; NotFound: boolean;
+begin
+
+  Result:='';
+
+  for i:=1 to Length(Str) do begin
+    NotFound:=true;
+    for j:=0 to TableRead.Count - 1 do begin
+      KeyBytes:=Copy(TableRead.Strings[j], 1, Pos('=', TableRead.Strings[j]) - 1);
+      BytesValue:=Copy(TableRead.Strings[j], Length(KeyBytes) + 2, Length(TableRead.Strings[j]) - Length(KeyBytes) - 1);
+
+      if Str[i] = BytesValue then begin
+        Result:=Result + KeyBytes;
+        NotFound:=false;
+        break;
+      end;
+    end;
+    if NotFound then ShowMessage('Буква не найдена во ReadTable: ' + Str[i]);
+  end;
+end;
+
+function StrToBytesTBWrite(Str: string): string;
+var
+  i, j: integer; KeyBytes, BytesValue: string; NotFound: boolean;
+begin
+  Result:='';
+
+
+  for i:=1 to Length(Str) do begin
+    NotFound:=true;
+    for j:=0 to TableWrite.Count - 1 do begin
+      KeyBytes:=Copy(TableWrite.Strings[j], 1, Pos('=', TableWrite.Strings[j]) - 1);
+      BytesValue:=Copy(TableWrite.Strings[j], Length(KeyBytes) + 2, Length(TableWrite.Strings[j]) - Length(KeyBytes) - 1);
+
+      if Str[i] = BytesValue then begin
+        Result:=Result + KeyBytes;
+        NotFound:=false;
+        break;
+      end;
+    end;
+    if NotFound then ShowMessage('Буква не найдена во WriteTable: ' + Str[i]);
+  end;
+end;
+
+function HexToBytes(const hex: string): TByteArray;
+var
+  i: Integer;
+begin
+  Assert(Length(hex) mod 2 = 0, 'Hex string length must be even');
+  
+  SetLength(Result, Length(hex) div 2);
+  for i := 1 to Length(hex) div 2 do
+  begin
+    Result[i - 1] := StrToInt('$' + Copy(hex, (i - 1) * 2 + 1, 2));
+  end;
+end;
+
+procedure TMain.Button1Click(Sender: TObject);
+var
+  InputFile: file of byte;
+  CurByte: byte;
+  i, Line: integer;
+  FileStr: string;
+
+  byteArray: TByteArray;
+  fileStream: TFileStream;
+begin
+  //oldArray := HexToBytes(StrToBytesTBRead('Files'));
+  //newArray := HexToBytes(StrToBytesTBWrite('Бумаг'));
+
+  //
+
+  FileStr:='';
+  AssignFile(InputFile, ExtractFilePath(ParamStr(0)) + 'ST103-orig.diff');
+  Reset(InputFile);
+  for i:=0 to FileSize(InputFile) - 1 do begin
+    Seek(InputFile, i);
+    Read(InputFile, CurByte);
+    FileStr:=BytesToStr(CurByte) + FileStr;
+  end;
+  CloseFile(InputFile);
+
+  byteArray := HexToBytes( StringReplace(FileStr, StrToBytesTBRead('File'), StrToBytesTBWrite('Бума'), []) );
+  fileStream := TFileStream.Create(ExtractFilePath(ParamStr(0)) + 'ST103.diff', fmCreate);
+  try
+    fileStream.Write(byteArray[0], Length(byteArray));
+  finally
+    fileStream.Free;
+  end;
+end;
+
+procedure TMain.Button2Click(Sender: TObject);
+begin
+  MessageBox(0, 'Версия программы: 23.08.24', 'Патчер', MB_ICONINFORMATION);
+end;
+
+
+procedure TMain.OwnLineEditChange(Sender: TObject);
+begin
+  CheckSameLines;
+end;
+
+procedure TMain.CheckSameLines;
+begin
+  if Length(OriginalLineEdit.Text) <> Length(OwnLineEdit.Text) then begin
+    Label1.Font.Color:=clRed;
+    Label1.Caption:='Строки разные';
+  end else begin
+    Label1.Font.Color:=clGreen;
+    Label1.Caption:='Строки одинаковые';
+  end;
+  SizeFirstLineLbl.Caption:=IntToStr(Length(OriginalLineEdit.Text));
+  SizeSecondLineLbl.Caption:=IntToStr(Length(OwnLineEdit.Text));
+end;
+
+procedure TMain.Button3Click(Sender: TObject);
+var
+  SR: TSearchRec; i: integer; Dir: string;
+begin
+  ListBox1.Clear;
+  Dir:=PathEdt.Text;
+  if Dir[Length(Dir)] <> '\' then Dir:=Dir + '\';
+
+  //Поиск файлов
+  if FindFirst(Dir + '*.txt', faAnyFile, SR) = 0 then begin
+    repeat
+      Application.ProcessMessages;
+      if (SR.Name <> '.') and (SR.Name <> '..') then
+        if (SR.Attr and faDirectory) <> faDirectory then begin
+
+            ListBox1.Items.Add(SR.Name);
+
+        end;
+
+
+    until FindNext(SR) <> 0;
+
+  end;
+  FindClose(SR);
+  ProgressBar1.Max:=ListBox1.Items.Count - 1;
+end;
+
+function LoadTextFile(const FileName: string): string;
+var
+  Strings: TStringList;
+begin
+  Strings := TStringList.Create;
+  try
+    try
+      Strings.LoadFromFile(FileName, TEncoding.UTF8);
+    except
+      Strings.LoadFromFile(FileName);
+    end;
+    Result:=Strings.Text;
+  finally
+    Strings.Free;
+  end;
+end;
+
+procedure TMain.ListBox1Click(Sender: TObject);
+begin
+  if ListBox1.ItemIndex = -1 then Exit;
+  ListBox2.Items.Text:=LoadTextFile(PathEdt.Text + ListBox1.Items.Strings[ListBox1.ItemIndex]);
+  FileNameEdt.Text:=ListBox1.Items.Strings[ListBox1.ItemIndex];
+  ProgressBar1.Position:=ListBox1.ItemIndex;
+  Caption:='Перевод Dino Crisis ' + FloatToStr((ListBox1.ItemIndex + 1) * 100 div ListBox1.Items.Count) + '%';
+end;
+
+procedure TMain.ListBox2Click(Sender: TObject);
+var
+  i: integer;
+begin
+  if (ListBox2.ItemIndex = -1) or (ListBox2.ItemIndex = ListBox2.Count) or (ListBox2.ItemIndex = ListBox2.Count - 1) then Exit;
+
+  Label3.Caption:=IntToStr(ListBox2.ItemIndex + 1);
+
+  ListBox2.Items.Strings[ListBox2.ItemIndex]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex], '{--}', '/', [rfReplaceAll]);
+  if Pos('/', ListBox2.Items.Strings[ListBox2.ItemIndex]) > 0 then begin
+    Label2.Caption:='Ручное редактирование';
+    Label2.Font.Color:=clRed;
+  end else begin
+    Label2.Caption:='Авто вставка';
+    Label2.Font.Color:=clGreen;
+  end;
+
+  if ListBox2.Items.Strings[ListBox2.ItemIndex][1] = '{' then begin
+    ListBox2.Items.Strings[ListBox2.ItemIndex]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex], '\n', '\', [rfReplaceAll]);
+    //ListBox2.Items.Strings[ListBox2.ItemIndex]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex], '{$1001}', '', [rfReplaceAll]);
+    //ListBox2.Items.Strings[ListBox2.ItemIndex]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex], '{$2000}', '', [rfReplaceAll]);
+    //ListBox2.Items.Strings[ListBox2.ItemIndex]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex], '{--}', '', [rfReplaceAll]);
+    //ListBox2.Items.Strings[ListBox2.ItemIndex]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex], '{$0800}', '', [rfReplaceAll]);
+    ListBox2.Items.Strings[ListBox2.ItemIndex]:=RemoveBrackets(ListBox2.Items.Strings[ListBox2.ItemIndex]);
+
+    ListBox2.Items.Strings[ListBox2.ItemIndex + 1]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex + 1], '\n', '\', [rfReplaceAll]);
+    //ListBox2.Items.Strings[ListBox2.ItemIndex + 1]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex + 1], '{$1001}', '', [rfReplaceAll]);
+    //ListBox2.Items.Strings[ListBox2.ItemIndex + 1]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex + 1], '{$2000}', '', [rfReplaceAll]);
+    //ListBox2.Items.Strings[ListBox2.ItemIndex + 1]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex + 1], '{--}', '', [rfReplaceAll]);
+    //ListBox2.Items.Strings[ListBox2.ItemIndex + 1]:=StringReplace(ListBox2.Items.Strings[ListBox2.ItemIndex + 1], '{$0800}', '', [rfReplaceAll]);
+    ListBox2.Items.Strings[ListBox2.ItemIndex + 1]:=RemoveBrackets(ListBox2.Items.Strings[ListBox2.ItemIndex + 1]);
+  end;
+
+  OriginalLineEdit.Text:=ListBox2.Items.Strings[ListBox2.ItemIndex];
+  OwnLineEdit.Text:=ListBox2.Items.Strings[ListBox2.ItemIndex + 1];
+
+  if Length(OwnLineEdit.Text) < Length(OriginalLineEdit.Text) then
+    for i:=1 to Length(OriginalLineEdit.Text)- Length(OwnLineEdit.Text) do
+      OwnLineEdit.Text:=OwnLineEdit.Text + ' ';
+
+  //OriginalLineEdit.Text:=StrToBytesTBRead(ListBox2.Items.Strings[ListBox2.ItemIndex]);
+  //OwnLineEdit.Text:=StrToBytesTBWrite(ListBox2.Items.Strings[ListBox2.ItemIndex + 1]);
+  
+  //OriginalLineEdit.Text:=StrToBytesTBRead('Files are scattered.');
+  //OwnLineEdit.Text:=StrToBytesTBWrite('Бумаги разбросаны  .');
+end;
+
+procedure TMain.ToHexBtnClick(Sender: TObject);
+begin
+  TempBufStr1:=OriginalLineEdit.Text;
+  TempBufStr2:=OwnLineEdit.Text;
+  OriginalLineEdit.Text:=StrToBytesTBRead(OriginalLineEdit.Text);
+  OwnLineEdit.Text:=StrToBytesTBWrite(OwnLineEdit.Text);
+end;
+
+procedure TMain.CopyBtnClick(Sender: TObject);
+begin
+  ClipBoard.AsText:=OriginalLineEdit.Text;
+  OriginalLineEdit.Text:='';
+end;
+
+procedure TMain.CopyBtn2Click(Sender: TObject);
+begin
+  ClipBoard.AsText:=OwnLineEdit.Text;
+  OwnLineEdit.Text:='';
+end;
+
+procedure TMain.CopyHexBtnClick(Sender: TObject);
+begin
+  ClipBoard.AsText:=StrToBytesTBRead(OriginalLineEdit.Text);
+end;
+
+procedure TMain.ReturnStrsBtnClick(Sender: TObject);
+begin
+  OriginalLineEdit.Text:=TempBufStr1;
+  OwnLineEdit.Text:=TempBufStr2;
+end;
+
+procedure TMain.Button8Click(Sender: TObject);
+var
+  //InputFile, OutputFile: file of byte;
+  //CurByte, CurByte2: byte;
+  //i: integer;
+
+  FileStr: string;
+
+  InputFile, OutputFile: TFileStream;
+  Buffer: array[0..1] of Byte;
+
+
+  oldArray, newArray: TByteArray;
+
+  FoundFirstSymb: boolean;
+  SymbMatch: integer;
+begin
+  //ABCDE___A
+  //Тест12
+
+  oldArray := HexToBytes(StrToBytesTBRead('AB'));
+  newArray := HexToBytes(StrToBytesTBWrite('Те'));
+
+  SymbMatch:=0;
+  FoundFirstSymb:=false;
+
+  InputFile := TFileStream.Create('test ABCDE__A.bin', fmOpenRead);
+  OutputFile := TFileStream.Create('test Тест12.bin', fmCreate);
+  try
+    while InputFile.Read(Buffer, SizeOf(Buffer)) > 0 do
+      begin
+        {if FoundFirstSymb
+
+
+
+        if (oldArray[0] = Buffer[0]) and (oldArray[1] = Buffer[1]) then begin // Первый символ схожий
+          FoundFirstSymb:=true;
+          SymbMatch:=1;
+          //ShowMessage(IntToHex(Buffer[0], 2) + IntToHex(Buffer[1], 2));
+
+        end;   }
+
+
+
+        //OutputFile.Write(Buffer, SizeOf(Buffer));
+
+      end;
+    finally
+      InputFile.Free;
+      OutputFile.Free;
+    end;
+
+
+{
+var
+  oldArray, newArray: TByteArray;
+
+  fileStream, fileStream2: TFileStream;
+  buffer1: TByteArray;
+  bytesRead: Integer;
+begin
+  oldArray := HexToBytes(StrToBytesTBRead('Files'));
+  newArray := HexToBytes(StrToBytesTBWrite('Бумаг'));
+
+  //
+  fileStream2 := TFileStream.Create(ExtractFilePath(ParamStr(0)) + 'ST103.diff', fmCreate); // Открываем файл для записи
+
+  fileStream := TFileStream.Create(ExtractFilePath(ParamStr(0)) + 'ST103-orig.diff', fmOpenRead);
+  try
+    SetLength(buffer1, Length(oldArray));
+
+    repeat
+      bytesRead := fileStream.Read(buffer1[0], Length(oldArray));
+
+      if (bytesRead > 0) then
+      begin
+        // Сравниваем байты
+        if CompareMem(@buffer1[0], @oldArray[0], bytesRead) then
+        begin
+          ShowMessage('Найдено');
+          // Блоки совпадают
+          fileStream2.Write(newArray[0], Length(newArray));
+        end
+        else
+        begin
+          // Блоки различаются
+          fileStream2.Write(buffer1[0], bytesRead);
+        end;
+      end;
+    until (bytesRead <= 0);
+  finally
+    fileStream.Free;
+    fileStream2.Free;
+  end;
+}
+end;
+
+procedure TMain.OriginalLineEditChange(Sender: TObject);
+begin
+  CheckSameLines;
+end;
+
+procedure TMain.RemSpacesClick(Sender: TObject);
+begin
+  OwnLineEdit.Text := TrimRight(OwnLineEdit.Text);
+end;
+
+procedure TMain.FormClose(Sender: TObject; var Action: TCloseAction);
+var
+  Ini: TIniFile;
+begin
+  if ListBox1.ItemIndex = -1 then Exit;
+  Ini:=TIniFile.Create(ExtractFilePath(ParamStr(0)) + 'Config.ini');
+  Ini.WriteString('Main', 'LastFile', ListBox1.Items.Strings[ListBox1.ItemIndex]);
+  Ini.Free;
+end;
+
+procedure TMain.AddSpacesClick(Sender: TObject);
+var
+  i: integer;
+begin
+  if Length(OwnLineEdit.Text) < Length(OriginalLineEdit.Text) then
+    for i:=1 to Length(OriginalLineEdit.Text)- Length(OwnLineEdit.Text) do
+      OwnLineEdit.Text:=OwnLineEdit.Text + ' ';
+end;
+
+procedure TMain.SaveStrsBtnClick(Sender: TObject);
+begin
+  StrLine1:=OriginalLineEdit.Text;
+  StrLine2:=OwnLineEdit.Text;
+end;
+
+procedure TMain.ReturnStrs2BtnClick(Sender: TObject);
+begin
+  OriginalLineEdit.Text:=StrLine1;
+  OwnLineEdit.Text:=StrLine2;
+end;
+
+procedure TMain.Label3Click(Sender: TObject);
+var
+  Value: integer;
+begin
+  Value:=StrToInt(Label3.Caption);
+  if Value < ListBox2.Count then
+    ListBox2.ItemIndex:=Value - 1;
+end;
+
+end.
